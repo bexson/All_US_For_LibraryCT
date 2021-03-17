a9ac65e2acdbe1621710f741771c2fd37a42b8f9
Beka <bektemir.abdymomunov@mail.ru>
2021-03-16 23:48:47 -0400

    added some dependencies for my test cases

---
+++ src/test/java/com/libraryAutomation/step_definitions/Info_About_Library_StepDefs.java
@@ -0,0 +1,26 @@
+package com.libraryAutomation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Info_About_Library_StepDefs {
+
+    @Given("librarian logged in")
+    public void librarian_logged_in() {
+
+    }
+
+
+    @When("librarian is on the landing page")
+    public void librarian_is_on_the_landing_page() {
+
+    }
+
+    @Then("librarian should see the total number of users")
+    public void librarian_should_see_the_total_number_of_users() {
+
+    }
+
+
+}
